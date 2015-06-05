aaf6a00fed3c654e14472325c21dfc7cb0461a32
Carlos Gonzales <carlos@pnfsoftware.com>
2015-06-05 13:07:39 -0700

    Add utility class

---
+++ src/com/pnf/Utils.java
@@ -0,0 +1,8 @@
+package com.pnf;
+
+import com.pnfsoftware.jeb.util.logging.GlobalLog;
+import com.pnfsoftware.jeb.util.logging.ILogger;
+
+public class Utils {
+	public static ILogger LOG = GlobalLog.getLogger(Utils.class);
+}
