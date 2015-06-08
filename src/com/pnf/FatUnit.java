c890e0d205ebe0bae91abd85cfe4d661074e7e1b
Carlos Gonzales <carlos@pnfsoftware.com>
2015-06-08 16:57:28 -0700

    Remove unnecessary file dumping since we are doing everything in memory

--- src/com/pnf/FatUnit.java
+++ src/com/pnf/FatUnit.java
@@ -1,7 +1,5 @@
 package com.pnf;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import com.pnf.FatCore.FileOutputEntry;
@@ -9,7 +7,6 @@ import com.pnfsoftware.jeb.core.properties.IPropertyDefinitionManager;
 import com.pnfsoftware.jeb.core.units.AbstractBinaryUnit;
 import com.pnfsoftware.jeb.core.units.IUnit;
 import com.pnfsoftware.jeb.core.units.IUnitProcessor;
-import com.pnfsoftware.jeb.util.IO;
 
 public class FatUnit extends AbstractBinaryUnit {
 	private static final String FILE_TYPE = "fat_image";
@@ -19,13 +16,6 @@ public class FatUnit extends AbstractBinaryUnit {
 	}
 	
 	public boolean process(){
-		File tempDir = null;
-		try {
-			tempDir = IO.createTempFolder(FILE_TYPE + "_data");
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to create temporary directory in fat_plugin");
-		}
-		
 		FatCore core = new FatCore(getBytes());
 		List<FileOutputEntry> files = core.getStoredFiles();
 		
@@ -33,8 +23,6 @@ public class FatUnit extends AbstractBinaryUnit {
 			IUnit child = getUnitProcessor().process(f.getFile().getName(), f.getBuffer().array(), this);
 			getChildren().add(child);
 		}
-		
-		core.dumpFiles(tempDir);
 		
 		return true;
 	}
