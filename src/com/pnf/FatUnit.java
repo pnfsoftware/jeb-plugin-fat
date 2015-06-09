2d1aadb0e784e9f10315adda4d413d53069a1c79
Carlos Gonzales <carlos@pnfsoftware.com>
2015-06-09 14:13:49 -0700

    Add FAT specific info to description and fire update event immediately after

--- src/com/pnf/FatUnit.java
+++ src/com/pnf/FatUnit.java
@@ -3,6 +3,8 @@ package com.pnf;
 import java.util.List;
 
 import com.pnf.FatProcessor.ImageFileEntry;
+import com.pnfsoftware.jeb.core.events.J;
+import com.pnfsoftware.jeb.core.events.JebEvent;
 import com.pnfsoftware.jeb.core.properties.IPropertyDefinitionManager;
 import com.pnfsoftware.jeb.core.units.AbstractBinaryUnit;
 import com.pnfsoftware.jeb.core.units.IUnit;
@@ -10,14 +12,35 @@ import com.pnfsoftware.jeb.core.units.IUnitProcessor;
 
 public class FatUnit extends AbstractBinaryUnit {
 	private static final String FILE_TYPE = "fat_image";
+	private StringBuffer desc;
+	private boolean descSet = false;
 
 	public FatUnit(String name, byte[] data, IUnitProcessor unitProcessor, IUnit parent, IPropertyDefinitionManager pdm){
 		super(null, data, FILE_TYPE, name, unitProcessor, parent, pdm);
 	}
 
+	public String getDescription(){
+		// First add superclass data
+
+		if(desc == null && descSet == false){
+			desc = new StringBuffer(super.getDescription());
+			desc.append("\n");
+		}
+
+		return desc.toString();
+	}
+
 	public boolean process(){
 		// Create a FatProcessor object that will handle interfacing with the FAT library
 		FatProcessor core = new FatProcessor(getBytes());
+
+		// Check if description is null
+		if(desc == null)
+			getDescription(); // Populate with data if it is
+
+		// Add FAT specific info to description
+		desc.append("Type := " + core.getFatType() + "\n");
+
 
 		// Retrieve a list of the files stored within the image we are processing
 		List<ImageFileEntry> files = core.getStoredFiles();
@@ -30,6 +53,9 @@ public class FatUnit extends AbstractBinaryUnit {
 			// Add newly created subunit to list of children
 			getChildren().add(child);
 		}
+
+		// Throw unit change event
+		notifyListeners(new JebEvent(J.UnitChange));
 
 		return true;
 	}
